9d30ffa9cbeea66ca653897ea591cb20207ca3d5
VirginiaMorera <morera.virginia@gmail.com>
2023-01-17 11:11:52 +0000

    Add helpful comments to data carpentry; Change months to labels; Add BADGER_STATUS to the vacc ds;

--- Scripts/data_carpentry_badgers.R
+++ Scripts/data_carpentry_badgers.R
@@ -15,52 +15,82 @@ badgers_clean <- badgers %>%
   # select columns to retain
   dplyr::select(-AGE, -GIRTH, -LENGTH, -DATE_SETT_LAST_CHECKED, -COST, -TOTAL_COST, -POSTMORTEM_CODE, 
                 -INVOICE_NO) %>% 
-  # clean up, date format, etc
+  # Turn date caught and entered into Date formats
   mutate(DATE_CAUGHT = parse_date(DATE_CAUGHT, "%b %d, %Y %H:%M:%S %p"), 
          DATE_ENTERED = parse_date(DATE_ENTERED, "%b %d, %Y %H:%M:%S %p"), 
+         # turn sex into categorical
          SEX = factor(SEX, levels = c("Female", "Male")), 
-         month_entered = month(DATE_ENTERED), 
+         # month and year of data entry
+         month_entered = month(DATE_ENTERED, label = TRUE), 
          year_entered = year(DATE_ENTERED), 
-         month_captured = month(DATE_CAUGHT), 
+         monthyear_ent = paste(month_entered, year_entered, sep = "-"), 
+         monthyear_ent = parse_date(monthyear_ent, "%b-%Y"), 
+         # month and year of capture
+         month_captured = month(DATE_CAUGHT, label = TRUE), 
          year_captured = year(DATE_CAUGHT), 
-         monthday_ent = paste(month_entered, year_entered, sep = "-"), 
-         monthday_ent = parse_date(monthday_ent, "%m-%Y"), 
-         monthday_capt = paste(month_captured, year_captured, sep = "-"), 
-         monthday_capt = parse_date(monthday_capt, "%m-%Y"), 
+         monthdyear_capt = paste(month_captured, year_captured, sep = "-"), 
+         monthyear_capt = parse_date(monthdyear_capt, "%b-%Y"), 
+         # add columns to match with the vaccination ds
          BADGER_ACTION = "REMOVED", 
          VACCINATION = "N", 
          PROGRAMME = "Culling", 
+         BADGER_STATUS = "Culled",
+         # turn weight into NA if it's 0
          WEIGHT = na_if(WEIGHT, 0))
 
 badgers_vacc_clean <- badgers_vacc %>% 
+  # select necessary variables
   dplyr::select(BADGER_ID, SETT_ID, SEX, AGE, VACCINATION, WEIGHT,
-                ECTOPARASITES, 
+                ECTOPARASITES, BADGER_STATUS,
                 ECTOPARASITE_TYPE, BADGER_ACTION, DATE_ENTERED, 
                 DATE_CAUGHT = DATE_CAPTURE) %>% 
+  # modify date caught 
   mutate(DATE_CAUGHT = parse_date(DATE_CAUGHT, "%b %d, %Y %H:%M:%S %p"), 
-         DATE_ENTERED = parse_date(DATE_ENTERED, "%b %d, %Y %H:%M:%S %p"),
-         month_entered = month(DATE_ENTERED), 
-         year_entered = year(DATE_ENTERED), 
-         month_captured = month(DATE_CAUGHT), 
+         month_captured = month(DATE_CAUGHT, label = TRUE), 
          year_captured = year(DATE_CAUGHT), 
-         monthday_ent = paste(month_entered, year_entered, sep = "-"), 
-         monthday_ent = parse_date(monthday_ent, "%m-%Y"), 
-         monthday_capt = paste(month_captured, year_captured, sep = "-"), 
-         monthday_capt = parse_date(monthday_capt, "%m-%Y"),  
+         monthdyear_capt = paste(month_captured, year_captured, sep = "-"), 
+         monthdyear_capt = parse_date(monthdyear_capt, "%b-%Y"),
+         # modify date entered
+         DATE_ENTERED = parse_date(DATE_ENTERED, "%b %d, %Y %H:%M:%S %p"),
+         month_entered = month(DATE_ENTERED, label = TRUE), 
+         year_entered = year(DATE_ENTERED), 
+         monthyear_ent = paste(month_entered, year_entered, sep = "-"), 
+         monthyear_ent = parse_date(monthyear_ent, "%b-%Y"),   
+         # clean and recode Sex
          SEX = recode(SEX, 'F' = "Female", 'FEMALE' = "Female", 
                       'M' = "Male", 'MALE' = "Male", 
                       'X' = NA_character_), 
+         # clean and recode Age
          AGE = recode(AGE, 'adult' = "Adult", 'ADULT' = "Adult",'CUB' = "Cub", 
                       'JUVENILE' = "Juvenile", 'OLD' = "Old", 
                       '9' = NA_character_), 
+         # clean and recode badger status
+         BADGER_STATUS = recode(BADGER_STATUS, '2014 badger' = "2014_badger", 
+                                'Ann Barber' = "AnnBarber", 'BADGER' = "Badger",
+                                'EPI Removed Under Permission' = "Deceased", 
+                                'Euthanised by V' = "Deceased", 
+                                'Goodger not on' = "Goodger", 
+                                'new' = "New",
+                                'NEW' = "New",
+                                'Old thin' = NA_character_, 
+                                'poor condition' = NA_character_,
+                                'RECAPTURE' = "Recapture", 
+                                'Recapture (14 days)' = "Recapture",
+                                'Recapture (30 days)' = "Recapture",
+                                'removed' = "Deceased",
+                                'road kill' = "Deceased",
+                                'roadside injury' = NA_character_), 
+         # turn SETT to numeric to match other ds
          SETT_ID = as.numeric(SETT_ID), 
+         # id programme
          PROGRAMME = "Vaccination", 
+         # clean weight variable
          WEIGHT = na_if(WEIGHT, 0))
 
 
 badgers_all <- bind_rows(badgers_clean, badgers_vacc_clean)
 
-saveRDS(badgers_all, file = "Data/badgers_all.RDS")
+# saveRDS(badgers_all, file = "Data/badgers_all.RDS")
 
 ##########################################
 #### Visualise badger individual data ####
@@ -68,10 +98,10 @@ saveRDS(badgers_all, file = "Data/badgers_all.RDS")
 
 # Date captured
 ggplot(badgers_all) + 
-  geom_bar(aes(x = monthday_capt, fill = as.factor(month_captured)), 
-           stat = "count") + 
+  geom_bar(aes(x = monthyear_capt, fill = month_captured)) + 
   scale_x_date(date_breaks = "1 year",date_labels = "%Y") + 
   labs(x = "Date", y = "Number of badgers", fill = "Month") + 
+  scale_fill_discrete(na.value = "gray") + 
   theme_bw() + 
   ggtitle("Date of capture")
 
@@ -80,7 +110,7 @@ table(is.na(badgers_clean$DATE_CAUGHT))
 
 # Date entered
 ggplot(badgers_all) + 
-  geom_bar(aes(x = monthday_ent, fill = as.factor(month_entered)), 
+  geom_bar(aes(x = monthyear_ent, fill = as.factor(month_entered)), 
            stat = "count") + 
   scale_x_date(date_breaks = "1 year",date_labels = "%Y") + 
   scale_fill_discrete(breaks=seq(1, 12, 1), 
@@ -92,14 +122,14 @@ ggplot(badgers_all) +
   ggtitle("Date entered")
 
 # Month of capture
-ggplot(badgers_all) + 
-  geom_bar(aes(x = month_captured, fill = as.factor(month_captured)), 
+badgers_all %>% 
+  filter(!is.na(month_captured)) %>%
+  ggplot + 
+  geom_bar(aes(x = month_captured, 
+               fill = month_captured), 
            stat = "count") + 
   theme_bw() + 
-  scale_x_continuous(breaks=seq(1, 12, 1), 
-                     labels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun",
-                                "Jul", "Aug", 
-                                "Sep", "Oct", "Nov", "Dec")) + 
+  scale_fill_discrete() + 
   labs(x = "Month", y = "Number of badgers", fill = "Month") +
   ggtitle("Capture month")
 #Biased because 56328 capture dates missing. Also, is this biased by fieldwork personnel holidays, farm working days, etc? 
@@ -109,7 +139,7 @@ badgers_all %>%
   filter(WEIGHT < 20) %>%
   filter(!is.na(month_captured)) %>% 
   ggplot + 
-  geom_histogram(aes(x = WEIGHT))
+  geom_histogram(aes(x = WEIGHT), binwidth = 0.5)
 
 # weight by month
 badgers_all %>% 
@@ -162,6 +192,17 @@ badgers_all %>%
   labs(x = "Year", y = "Number of badgers", fill = "Badger's destiny") +
   ggtitle("Badger destiny")
 
+# badger destiny (including categories in vacc dataset)
+badgers_all %>% 
+  filter(!is.na(BADGER_STATUS)) %>% 
+  ggplot + 
+  geom_bar(aes(x = year_entered, fill = BADGER_STATUS), stat = "count") + 
+  theme_bw() + 
+  scale_x_continuous(breaks=seq(2007, 2022, 1)) +
+  labs(x = "Year", y = "Number of badgers", fill = "Badger's destiny") +
+  ggtitle("Badger destiny")
+
+
 # sex ratio
 badgers_all %>% 
   # filter(!is.na(SEX)) %>% 
@@ -196,3 +237,4 @@ badgers_all %>%
   geom_boxplot(aes(x = AGE, fill = AGE, y = WEIGHT)) + 
   theme_bw() + 
   ggtitle("Weight by age")
+
