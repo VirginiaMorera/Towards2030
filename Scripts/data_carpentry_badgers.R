b97822ec3e546f5dd865cd940e5b315fa221c4d2
VirginiaMorera <morera.virginia@gmail.com>
2023-11-21 17:55:57 +0000

    redo the cleaning and data carpentry after receiving new DAFM data

--- Scripts/data_carpentry_badgers.R
+++ Scripts/data_carpentry_badgers.R
@@ -1,61 +1,97 @@
 rm(list = ls())
 source("Scripts/setup.R")
 
-#### Load and clean badger individual data ####
-
-# load 
-badgers <- read_csv("Data/Raw/forR_captured_badgers.csv")
-badgers_vacc <- read_csv("Data/Raw/forR_vacc_badgers_names_removed.csv")
 ireland <- st_read("Data/Other/ireland_ITM.shp")
+
+# Load and clean badger individual data ####
+
+## Culling programme badgers ####
+
+### Load and clean badger data ####
+badgers <- read_xlsx("Data/Raw/tbl_captured_badgers_2023.xlsx")
 
 # remove unnecessary columns and clean
 badgers_clean <- badgers %>% 
   # select columns to retain
   dplyr::select(-AGE, -GIRTH, -LENGTH, -DATE_SETT_LAST_CHECKED, -COST, 
-                -TOTAL_COST, -POSTMORTEM_CODE, -INVOICE_NO) %>% 
-  # Turn date caught and entered into Date formats
-  mutate(DATE_CAUGHT = parse_date(DATE_CAUGHT, "%b %d, %Y %H:%M:%S %p"), 
-         DATE_ENTERED = parse_date(DATE_ENTERED, "%b %d, %Y %H:%M:%S %p"), 
-         # turn sex into categorical
-         SEX = factor(SEX, levels = c("Female", "Male")), 
-         # month and year of data entry
-         month_entered = month(DATE_ENTERED, label = TRUE), 
-         year_entered = year(DATE_ENTERED), 
-         monthyear_ent = paste(month_entered, year_entered, sep = "-"), 
-         # monthyear_ent = parse_date(monthyear_ent, "%b-%Y"), 
-         # month and year of capture
-         month_captured = month(DATE_CAUGHT, label = TRUE), 
-         year_captured = year(DATE_CAUGHT), 
-         monthdyear_capt = paste(month_captured, year_captured, sep = "-"), 
-         # monthyear_capt = parse_date(monthdyear_capt, "%b-%Y"), 
-         # add columns to match with the vaccination ds
-         BADGER_ACTION = "REMOVED", 
-         VACCINATION = "N", 
-         PROGRAMME = "Culling", 
-         BADGER_STATUS = "Culled",
-         # turn weight into NA if it's 0
-         WEIGHT = na_if(WEIGHT, 0))
+                -TOTAL_COST, -POSTMORTEM_CODE, -INVOICE_NO, -DATE_ENTERED) %>% 
+  mutate(
+    # remove weird ' from badger names
+    across('BADGER_ID', str_replace, "`", ""),
+    # replace  \ with / in capture block event
+    across('CAPTURE_BLOCK_EVENT', str_replace, "\\\\", "/"),
+    # turn sex into categorical
+    SEX = recode(SEX, D = NA_character_, Female = "Female", Male = "Male", 
+                 Unknown = NA_character_, Unknownm = NA_character_),
+    SEX = factor(SEX, levels = c("Female", "Male")), 
+    # turn weight into NA if it's 0
+    WEIGHT = na_if(WEIGHT, 0))
 
-badgers_vacc_clean <- badgers_vacc %>% 
+### Load and clean capture block event data ####
+capture_events <- read_xlsx("Data/Raw/tbl_capture_EVENTS_2023.xlsx")
+
+capture_events %<>%
+  mutate(event_duration = DATE_COMPLETED - DATE_COMMENCED) %>% 
+  select(-diff, -diff2)
+
+### Add capture dates to badger data and clean mistakes ####
+badgers_clean_with_date <- badgers_clean %>% 
+  left_join(capture_events)
+
+## Events where the number of badgers captured field doesn't match the number of 
+#  badgers in the dataset  
+
+unmatched_cap_events <- badgers_clean_with_date %>% 
+  group_by(CAPTURE_BLOCK_EVENT) %>% 
+  summarise(n_rows = n(),
+            badgers_captured = unique(TOTAL_BADGERS), 
+            diff = n_rows - badgers_captured) %>% 
+  filter(!is.na(badgers_captured)) %>% # remove events where number of badgers wasn't recorded, we'll have to trust the number of rows
+  # mutate(diff = replace_na(diff, 0)) %>% 
+  filter(diff != 0) %>% # remove events where badgers captured matches no rows
+  filter(abs(diff) != 1) %>% # remove events where the difference is just 1 (close enough)
+  filter(diff %!in% -2:-5) # if we're missing 2 to 5 rows we might accept it
+  
+badgers_clean_with_date <- badgers_clean_with_date %>% 
+  filter(CAPTURE_BLOCK_EVENT %!in% unmatched_cap_events$CAPTURE_BLOCK_EVENT)
+
+## Events where the capture date doesn't fall within the dates of the capture event
+just_date <- badgers_clean_with_date %>% 
+  select(BADGER_ID, DATE_CAUGHT, DATE_COMMENCED, DATE_COMPLETED, CAPTURE_BLOCK_EVENT) %>% 
+  mutate(diff_time = difftime(DATE_COMPLETED, DATE_COMMENCED, units = "days"), 
+         checks = if_else(DATE_CAUGHT >= DATE_COMMENCED & DATE_CAUGHT <= DATE_COMPLETED, 
+                          "Yes", "No")) %>% 
+  filter(checks == "No")
+  
+badgers_cleaner_with_date <- badgers_clean_with_date %>% 
+  filter(CAPTURE_BLOCK_EVENT %!in% just_date$CAPTURE_BLOCK_EVENT)
+
+c_badgers <- badgers_cleaner_with_date %>% 
+  select(SETT_ID:DATE_COMPLETED) %>% 
+  mutate( 
+    # add columns to match with the vaccination ds
+    BADGER_ACTION = "REMOVED", 
+    VACCINATION = "N", 
+    PROGRAMME = "Culling", 
+    BADGER_STATUS = "Culled", 
+    DATE_EST =  DATE_COMMENCED + floor(difftime(DATE_COMPLETED, DATE_COMMENCED, units = "days")/2),
+    DATE_CAUGHT = if_else(is.na(DATE_CAUGHT), DATE_EST, DATE_CAUGHT))
+  
+## Vaccination programme badgers ####
+
+### Load and clean badger data ####
+
+badgers_vacc <- read_xlsx("Data/Raw/tbl_vacc_badgers_2023.xlsx")
+
+v_badgers <- badgers_vacc %>% 
   # select necessary variables
-  dplyr::select(BADGER_ID, SETT_ID, SEX, AGE, VACCINATION, WEIGHT,
+  dplyr::select(SETT_ID, BADGER_ID, DATE_CAUGHT = DATE_CAPTURE, SEX, AGE, WEIGHT,
+                VACCINATION, 
                 ECTOPARASITES, BADGER_STATUS,
                 ECTOPARASITE_TYPE, BADGER_ACTION, DATE_ENTERED, 
-                DATE_CAUGHT = DATE_CAPTURE) %>% 
+                ) %>% 
   # modify date caught 
-  mutate(DATE_CAUGHT = parse_date(DATE_CAUGHT, "%b %d, %Y %H:%M:%S %p"), 
-         month_captured = month(DATE_CAUGHT, label = TRUE), 
-         year_captured = year(DATE_CAUGHT), 
-         monthdyear_capt = paste(month_captured, year_captured, sep = "-"), 
-         # monthdyear_capt = parse_date(monthdyear_capt, "%b-%Y"),
-         # modify date entered
-         DATE_ENTERED = parse_date(DATE_ENTERED, "%b %d, %Y %H:%M:%S %p"),
-         month_entered = month(DATE_ENTERED, label = TRUE), 
-         year_entered = year(DATE_ENTERED), 
-         monthyear_ent = paste(month_entered, year_entered, sep = "-"), 
-         # monthyear_ent = parse_date(monthyear_ent, "%b-%Y"),   
-         # clean and recode Sex
-         SEX = recode(SEX, 'F' = "Female", 'FEMALE' = "Female", 
+  mutate(SEX = recode(SEX, 'F' = "Female", 'FEMALE' = "Female", 
                       'M' = "Male", 'MALE' = "Male", 
                       'X' = NA_character_), 
          # clean and recode Age
@@ -64,98 +100,77 @@ badgers_vacc_clean <- badgers_vacc %>%
                       '9' = NA_character_), 
          # clean and recode badger status
          BADGER_STATUS = recode(BADGER_STATUS, '2014 badger' = "2014_badger", 
+                                '2nd Chip' = NA_character_, 'clinical suspec' = NA_character_,
                                 'Ann Barber' = "AnnBarber", 'BADGER' = "Badger",
                                 'EPI Removed Under Permission' = "Deceased", 
+                                'Euthanised' = "Deceased", 
                                 'Euthanised by V' = "Deceased", 
                                 'Goodger not on' = "Goodger", 
                                 'new' = "New",
                                 'NEW' = "New",
+                                'Not on system' = NA_character_,
+                                'Not on System' = NA_character_,
                                 'Old thin' = NA_character_, 
                                 'poor condition' = NA_character_,
+                                'other' = NA_character_,
                                 'RECAPTURE' = "Recapture", 
                                 'Recapture (14 days)' = "Recapture",
                                 'Recapture (30 days)' = "Recapture",
+                                'Recaptured' = 'Recapture',
                                 'removed' = "Deceased",
                                 'road kill' = "Deceased",
                                 'roadside injury' = NA_character_), 
-         # turn SETT to numeric to match other ds
-         SETT_ID = as.numeric(SETT_ID), 
-         # id programme
+        # id programme
          PROGRAMME = "Vaccination", 
          # clean weight variable
          WEIGHT = na_if(WEIGHT, 0))
 
 
-badgers_all <- bind_rows(badgers_clean, badgers_vacc_clean)
-
-#### Create capture year from date captured and date entered ####
-
-table(is.na(badgers_all$year_captured))
-table(is.na(badgers_all$year_entered))
-
-badgers_all <- badgers_all %>% 
-  mutate(year_estimated = if_else(is.na(year_captured), 
-                                  year_entered, year_captured), 
-         year_data_origin = if_else(is.na(year_captured), 
-                                    "data_entry_date", "capture_date"))
-
-# saveRDS(badgers_all, file = "Data/badgers_all.RDS")
+badgers_all <- bind_rows(c_badgers, v_badgers) %>% 
+  select(SETT_ID, BADGER_ID, AGE, WEIGHT, SEX, DATE_CAUGHT, 
+         CAPTURE_BLOCK_EVENT, PROGRAMME) %>% 
+  mutate(
+    DATE_CAUGHT = as.Date(DATE_CAUGHT),
+    YEAR = as.factor(year(DATE_CAUGHT)), 
+    MONTH = as.factor(month(DATE_CAUGHT)), 
+    MONTHYEAR = ym(paste(YEAR, MONTH, sep = "_"))
+  )
+# saveRDS(badgers_all, file = "Data/badgers_all_2023.RDS")
 
 #### Visualise badger individual data ####
 
-badgers_all <- readRDS("Data/badgers_all.RDS")
+badgers_all <- readRDS("Data/badgers_all_2023.RDS")
 
 # Date captured
 ggplot(badgers_all) + 
-  geom_bar(aes(x = monthyear_capt, fill = month_captured)) + 
+  geom_bar(aes(x = DATE_CAUGHT, fill = MONTH, group = MONTH)) + 
   scale_x_date(date_breaks = "1 year",date_labels = "%Y") + 
   labs(x = "Date", y = "Number of badgers", fill = "Month") + 
   scale_fill_discrete(na.value = "gray") + 
   theme_bw() + 
   ggtitle("Date of capture")
 
-# more than half the records in the culling dataset don't have capture date
-table(is.na(badgers_clean$DATE_CAUGHT))
-
-# Date entered
 ggplot(badgers_all) + 
-  geom_bar(aes(x = monthyear_ent, fill = as.factor(month_entered)), 
-           stat = "count") + 
+  geom_bar(aes(x = MONTHYEAR, fill = MONTH, group = MONTH)) + 
   scale_x_date(date_breaks = "1 year",date_labels = "%Y") + 
-  scale_fill_discrete(breaks=seq(1, 12, 1), 
-                     labels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun",
-                                "Jul", "Aug", 
-                                "Sep", "Oct", "Nov", "Dec")) + 
-  labs(x = "Date", y = "Number of badgers", fill = "Month") + 
+  labs(x = "Month", y = "Number of badgers", fill = "Month") + 
+  scale_fill_discrete(na.value = "gray") + 
   theme_bw() + 
-  ggtitle("Date entered")
+  ggtitle("Date of capture")
 
-# Month of capture
-badgers_all %>% 
-  filter(!is.na(month_captured)) %>%
-  ggplot + 
-  geom_bar(aes(x = month_captured, 
-               fill = month_captured), 
-           stat = "count") + 
-  theme_bw() + 
-  scale_fill_discrete() + 
-  labs(x = "Month", y = "Number of badgers", fill = "Month") +
-  ggtitle("Capture month")
-#Biased because 56328 capture dates missing. Also, is this biased by fieldwork personnel holidays, farm working days, etc? 
 
 # weight histogram
 badgers_all %>% 
   filter(WEIGHT < 20) %>%
-  filter(!is.na(month_captured)) %>% 
   ggplot + 
-  geom_histogram(aes(x = WEIGHT), binwidth = 0.5)
+  geom_histogram(aes(x = WEIGHT), binwidth = 0.5) +
+  facet_wrap(~SEX)
 
 # weight by month
 badgers_all %>% 
   filter(WEIGHT < 20) %>% 
-  filter(!is.na(month_captured)) %>% 
   ggplot + 
-  geom_boxplot(aes(x = factor(month_captured), y = WEIGHT, fill = factor(month_captured))) + 
+  geom_boxplot(aes(x = factor(MONTH), y = WEIGHT, fill = factor(MONTH))) + 
   theme_bw() + 
   scale_x_discrete(breaks=seq(1, 12, 1), 
                      labels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun",
@@ -166,84 +181,15 @@ badgers_all %>%
                               "Jul", "Aug", 
                               "Sep", "Oct", "Nov", "Dec")) +
   labs(x = "Month of capture", y = "Weight (kg)", fill = "Month of capture") + 
+  facet_wrap(~SEX) + 
   ggtitle("Weight by month")
-
-# ectoparasites
-badgers_all %>% 
-  mutate(Parasite_data = if_else(is.na(ECTOPARASITE_TYPE), "No", 
-                                 if_else(ECTOPARASITE_TYPE == "N/A", "No", "Yes"))) %>% 
-  ggplot + 
-  geom_bar(aes(x = year_entered, fill = Parasite_data), stat = "count") + 
-  theme_bw() + 
-  scale_x_continuous(breaks=seq(2007, 2022, 1)) +
-  labs(x = "Year", y = "Number of badgers", fill = "Ectoparasite type collected") + 
-  theme(legend.position="bottom") + 
-  ggtitle("Collection of ectoparasite data")
 
 #programme
 badgers_all %>% 
   ggplot + 
-  geom_bar(aes(x = year_entered, fill = PROGRAMME), stat = "count") + 
+  geom_bar(aes(x = YEAR, fill = PROGRAMME), stat = "count") + 
   theme_bw() + 
-  scale_x_continuous(breaks=seq(2007, 2022, 1)) +
   labs(x = "Year", y = "Number of badgers", fill = "Programme") +
   ggtitle("Programme")
 
-
-# badger destiny
-badgers_all %>% 
-  filter(!is.na(BADGER_ACTION)) %>% 
-  mutate(BADGER_ACTION = factor(BADGER_ACTION, levels = c("REMOVED", "RELEASED"))) %>% 
-  ggplot + 
-  geom_bar(aes(x = year_entered, fill = BADGER_ACTION), stat = "count") + 
-  theme_bw() + 
-  scale_x_continuous(breaks=seq(2007, 2022, 1)) +
-  labs(x = "Year", y = "Number of badgers", fill = "Badger's destiny") +
-  ggtitle("Badger destiny")
-
-# badger destiny (including categories in vacc dataset)
-badgers_all %>% 
-  filter(!is.na(BADGER_STATUS)) %>% 
-  ggplot + 
-  geom_bar(aes(x = year_entered, fill = BADGER_STATUS), stat = "count") + 
-  theme_bw() + 
-  scale_x_continuous(breaks=seq(2007, 2022, 1)) +
-  labs(x = "Year", y = "Number of badgers", fill = "Badger's destiny") +
-  ggtitle("Badger destiny")
-
-
-# sex ratio
-badgers_all %>% 
-  # filter(!is.na(SEX)) %>% 
-  ggplot + 
-  geom_bar(aes(x = monthday_ent, fill = SEX), stats = "count") + 
-  theme_bw() + 
-  ggtitle("Sex ratio")
-
-# size by sex
-badgers_all %>% 
-  filter(!is.na(SEX)) %>%
-  filter(WEIGHT < 20) %>% 
-  ggplot + 
-  geom_boxplot(aes(x = SEX, fill = SEX, y = WEIGHT)) + 
-  theme_bw() + 
-  ggtitle("Weight by sex")
-
-# age
-badgers_all %>% 
-  filter(!is.na(AGE)) %>%
-  ggplot + 
-  geom_bar(aes(x = monthday_ent, fill = AGE), stats = "count") + 
-  theme_bw() + 
-  ggtitle("Sex ratio")
-
-# size by age
-badgers_all %>% 
-  filter(!is.na(AGE)) %>%
-  mutate(AGE = factor(AGE, levels = c("Old", "Adult", "Juvenile", "Cub"))) %>% 
-  filter(WEIGHT < 20) %>% 
-  ggplot + 
-  geom_boxplot(aes(x = AGE, fill = AGE, y = WEIGHT)) + 
-  theme_bw() + 
-  ggtitle("Weight by age")
 
