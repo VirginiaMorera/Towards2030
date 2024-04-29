b22c352bbaddda48418b4dd9f2e06a26e95f345b
VirginiaMorera <morera.virginia@gmail.com>
2024-04-29 14:08:13 +0100

    Adapt to create different resolution meshes

--- Scripts/creation_of_mesh.R
+++ Scripts/creation_of_mesh.R
@@ -4,8 +4,8 @@ source("Scripts/setup.R")
 #### Preparation ####
 
 ### Load map of ireland to use as boundary, and point data to check they're inside the boundary
-ireland <- st_read("Data/ireland_ITM.shp")
-sett_all <- readRDS("Data/sett_all.RDS")
+ireland <- st_read("Data/Other/ireland_ITM.shp")
+sett_all <- readRDS("Data/sett_all_2023.RDS")
 ireland %<>% st_transform(crs = projKM)
 sett_all %<>% st_transform(crs = projKM)
 
@@ -58,63 +58,41 @@ boundary_out$crs
 # The edge of the triangles in the inner mesh is 20km
 # The edge of the triangles in the outer mesh is 50km 
 
-# parameters from deer model (ish)
-mesh <- inla.mesh.2d(boundary = list(boundary_in, boundary_out), 
-                     max.edge = c(20, 100), 
-                     cutoff = 20, crs = boundary_out$crs)
-mesh$crs
+# parameters from deer model (ish) i.e. very large mesh 
+mesh_20k <- inla.mesh.2d(boundary = list(boundary_in, boundary_out), 
+                         max.edge = c(20, 100), 
+                         cutoff = 20, crs = boundary_out$crs)
 
-# parameters from Dambly et al 2023
+# parameters at same resolution as smoothed covars
+mesh_5k <- inla.mesh.2d(boundary = list(boundary_in, boundary_out), 
+                         max.edge = c(5, 100), 
+                         cutoff = 5, crs = boundary_out$crs)
 
-mesh <- inla.mesh.2d(boundary = list(boundary_in, boundary_out), 
-                     max.edge = c(45, 150), 
-                     cutoff = 9, crs = boundary_out$crs)
-mesh$crs
+# parameters at smaller resolution than smoothed covars
+mesh_2k <- inla.mesh.2d(boundary = list(boundary_in, boundary_out), 
+                        max.edge = c(2, 100), 
+                        cutoff = 2, crs = boundary_out$crs)
 
-
+mesh_1k <- inla.mesh.2d(boundary = list(boundary_in, boundary_out), 
+                        max.edge = c(1, 100), 
+                        cutoff = 1, crs = boundary_out$crs)
 #### Plot mesh ####
-
-plot(mesh)
-# plot(all_data_sp, add = T, col = "red")
+par(mfrow = c(2,2))
+plot(mesh_20k)
 plot(boundary_sp, border = "green", add = T)
 plot(boundary2_sp, border = "orange", add = T)
 
-## Creation of mask
-
-# This will allow us to hide the predictions on the outer boundary from our results (probably there's a better way but just in case)
-# This code is taken from Haakon Bakka's barrier model from the website https://haakonbakka.bitbucket.io/btopic107.html#4_barrier_models and it runs all at once
-
-### Code to create the mask
-
-tl = length(mesh$graph$tv[,1]) # Number of triangles of the mesh
-posTri = matrix(0, tl, 2) # matrix containing the coordinates of each triangle's 
-# we fill it with the vertex "coordinates here
-for (t in 1:tl) {
-  # Take the vertex of triangles
-  temp = mesh$loc[mesh$graph$tv[t, ], ]
-  # Compute barycenter which sintetize the triangle
-  posTri[t,] = colMeans(temp)[c(1,2)]
-}
-#transform it in Spatial Points
-posTri = SpatialPoints(posTri)
-posTri@proj4string <- CRS("NA")
-boundary_sp@proj4string <- CRS("NA")
-normal = over(boundary_sp, posTri, returnList = T) # this are the polygons contained in the inner boundary, the "good" ones
-normal = unlist(normal)
-barrier.triangles = setdiff(1:tl, normal) # this are the polygons of the outer boundary, the ones we want to hide
-# build a polygon that contains all polygons that are outer boundary and not inner. 
-poly.barrier = inla.barrier.polygon(mesh, barrier.triangles)
-
-### Plot the mask to check everything is ok
-plot(mesh)
-plot(poly.barrier, col = "gray", add = T)
-
+plot(mesh_5k)
+plot(mesh_2k)
+plot(mesh_1k)
+par(mfrow = c(1,1))
 
 # saveRDS(poly.barrier, file = "data/barrier.RDS")
-saveRDS(mesh, file = "Data/mesh.RDS")
-saveRDS(mesh, file = "Data/small_mesh.RDS")
-saveRDS(mesh, file = "Data/Dambly_mesh3.RDS")
+saveRDS(mesh_20k, file = "Data/Inla/mesh_20k.RDS")
+saveRDS(mesh_5k, file = "Data/Inla/mesh_5k.RDS")
+saveRDS(mesh_2k, file = "Data/Inla/mesh_2k.RDS")
+saveRDS(mesh_1k, file = "Data/Inla/mesh_1k.RDS")
 saveRDS(boundary2_sp, file = "Data/outer_boundary.RDS")
 saveRDS(boundary_sp, file = "Data/inner_boundary.RDS")
-saveRDS(poly.barrier, file = "Data/barrier.RDS")
+
 
