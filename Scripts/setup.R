56b2e2b046055f57b8bb1c24a84ce9755ee81957
VirginiaMorera <morera.virginia@gmail.com>
2024-09-30 18:24:12 +0100

    add function to scale 0 to 1

--- Scripts/setup.R
+++ Scripts/setup.R
@@ -60,3 +60,4 @@ list_to_stack <- function(raster_list, new_res, dest_crs = CRS("+proj=longlat"),
 projKM <- "+proj=tmerc +lat_0=53.5 +lon_0=-8 +k=0.99982 +x_0=600000 +y_0=750000
            +ellps=GRS80 +units=km +no_defs"
 
+scale01 <- function(x){(x-min(x))/(max(x)-min(x))}
