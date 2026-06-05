9f78343f54cfd61768f129b168987d50105e695c
Virginia <morera.virginia@gmail.com>
2026-06-05 14:18:58 +0100

    organise and fix index

--- Scripts/extract_covars.R
+++ Scripts/extract_covars.R
@@ -1,3 +1,4 @@
+# Housekeeping #### 
 rm(list = ls())
 source("Scripts/setup.R")
 
@@ -13,103 +14,6 @@ ireland_outline <- ireland %>%
   st_transform(crs = projKM)
 
 # saveRDS(ireland_outline, "Data/ireland_outline_km.RDS")
-
-# create empty 5km raster to resample ####
-
-largerRas <- raster(ext = extent(env_vars_km), crs = projKM, resolution = c(5,5))
-
-# Resample rasters that are factors ####
-
-##  Land use ####
-
-# 1 km
-landUse <- env_vars$landCover
-landUse_km <- projectRaster(landUse, crs = projKM, method = "ngb")
-
-landCover <- as(landUse_km, "SpatialPixelsDataFrame")
-landCover@data$landCover <- recode(landCover@data$landCover, 
-                                   '1' = "Builtup", '2' = "Saltwater_related", 
-                                   '3' = "No_vegetation", '4' = "Freshwater_related", 
-                                   '5' = "Other_vegetation", '6' = "Agricultural", 
-                                   '7' = "Pasture", '8' = "Grassland", '9' = "Transitional", 
-                                   '10' = "Coniferous_forest", '11' = "Mixed_forest", 
-                                   '12' = "Broadleaf_forest")
-
-table(landCover@data$landCover)
-
-saveRDS(landCover, file = "Data/landCover_1km.RDS")
-
-# 5 km
-
-landUse_5km <- resample(landUse_km, largerRas, method = "ngb")
-landCover_5km <- as(landUse_5km, "SpatialPixelsDataFrame")
-landCover_5km@data$landCover <- recode(landCover_5km@data$landCover, 
-                                   '1' = "Builtup", '2' = "Saltwater_related", 
-                                   '3' = "No_vegetation", '4' = "Freshwater_related", 
-                                   '5' = "Other_vegetation", '6' = "Agricultural", 
-                                   '7' = "Pasture", '8' = "Grassland", '9' = "Transitional", 
-                                   '10' = "Coniferous_forest", '11' = "Mixed_forest", 
-                                   '12' = "Broadleaf_forest")
-
-
-table(landCover_5km@data$landCover)
-
-saveRDS(landCover_5km, file = "Data/landCover_5km.RDS")
-
-##  Dominant leaf type ####
-
-# 1 km
-dlt <- env_vars$dominant_leaf_type
-
-dlt_km <- projectRaster(dlt, crs = projKM, method = "ngb")
-
-dlt_pix <- as(dlt_km, "SpatialPixelsDataFrame")
-
-dlt_pix@data$dominant_leaf_type <- recode(dlt_pix@data$dominant_leaf_type, 
-                                          '1' = "Broadleaf", '2' = "Coniferous")
-
-table(dlt_pix@data$dominant_leaf_type)
-
-saveRDS(dlt_pix, file = "Data/dlt_1km.RDS")
-
-# 5 km
-
-dlt_5km <- resample(dlt_km, largerRas, method = "ngb")
-dlt_pix_5km <- as(dlt_5km, "SpatialPixelsDataFrame")
-dlt_pix_5km@data$dominant_leaf_type <- recode(dlt_pix_5km@data$dominant_leaf_type, 
-                                              '1' = "Broadleaf", '2' = "Coniferous")
-
-
-table(dlt_pix_5km@data$dominant_leaf_type)
-
-saveRDS(dlt_pix_5km, file = "Data/dlt_5km.RDS")
-
-##  Forest type ####
-
-# 1 km
-ft <- env_vars$forest_type
-
-ft_km <- projectRaster(ft, crs = projKM, method = "ngb")
-
-ft_pix <- as(ft_km, "SpatialPixelsDataFrame")
-
-ft_pix@data$forest_type <- recode(ft_pix@data$forest_type, 
-                                          '0' = "Non-forest",  '1' = "Broadleaf", '2' = "Coniferous")
-
-table(ft_pix@data$forest_type, useNA = "always")
-
-saveRDS(ft_pix, file = "Data/ft_1km.RDS")
-
-# 5 km
-
-ft_5km <- resample(ft_km, largerRas, method = "ngb")
-ft_pix_5km <- as(ft_5km, "SpatialPixelsDataFrame")
-ft_pix_5km@data$forest_type <- recode(ft_pix_5km@data$forest_type, 
-                                      '0' = "Non-forest",  '1' = "Broadleaf", '2' = "Coniferous")
-
-table(ft_pix_5km@data$forest_type, useNA = "always")
-
-saveRDS(ft_pix_5km, file = "Data/ft_5km.RDS")
 
 # Calculate forest distance ####
 
@@ -133,12 +37,16 @@ env_vars_sel$forest_distances <- forest_distances
 writeRaster(env_vars_sel, filename = "Data/env_vars_1km.grd", format = "raster")
 
 ## resample to 5km and save ####
+
+largerRas <- raster(ext = extent(env_vars_km), crs = projKM, resolution = c(5,5))
 env_vars_sel_5km <- resample(env_vars_sel, largerRas, method = "bilinear")
 
 writeRaster(env_vars_sel_5km, filename = "Data/env_vars_5km.grd", format = "raster")
 
 
 # Corine continuous layers ####
+
+# these have been calculated in the server because it was too computationally heavy
 
 corine <- stack("Data/Covars/corine_ireland_1km.grd")
 env_vars_km <- stack("Data/Covars/env_vars_1km.grd")
@@ -153,54 +61,10 @@ writeRaster(all_covars_1km, filename = "Data/all_covars_1km.grd", format = "rast
             overwrite = T)
 
 plot(corineKM)
+
 corine_5k <- resample(corineKM, largerRas, method = "bilinear")
 plot(corine_5k)
 writeRaster(corine_5k, filename = "Data/corine_5km.grd", format = "raster", overwrite = T)
-
-# Smooth covariates to avoid issues with mesh resolution
-
-
-ireland_outline_sf <- readRDS("Data/Inla/ireland_outline_km.RDS")
-
-par(mfrow = c(1,3))
-r <- env_vars$tree_cover_density
-r2 <- focal(r, w= matrix(1,3,3), mean, na.rm = T)
-r3 <- focal(r, w= matrix(1,9,9), mean, na.rm = T)
-plot(r)
-plot(mesh2, add = T)
-plot(r2)
-plot(mesh2, add = T)
-plot(r3)
-plot(mesh2, add = T)
-
-env_vars_smooth3 <- stack()
-env_vars_smooth5 <- stack()
-
-for (i in 1:nlayers(all_covars_1km)) {
-  # i = 1
-  r <- all_covars_1km[[i]]
-  r2 <- focal(r, w= matrix(1,3,3), mean, na.rm = T)
-  r3 <- focal(r, w= matrix(1,5,5), mean, na.rm = T)
-  r2 <- crop(r2, ireland_outline_sf)
-  r3 <- crop(r3, ireland_outline_sf)
-  env_vars_smooth3 <- stack(env_vars_smooth3, r2)
-  env_vars_smooth5 <- stack(env_vars_smooth5, r3)
-}
-
-names(env_vars_smooth3) <- names(all_covars_1km)
-names(env_vars_smooth5) <- names(all_covars_1km)
-
-env_vars <- crop(all_covars_1km, ireland_outline_sf)
-plot(env_vars$tree_cover_density)
-plot(env_vars_smooth3$tree_cover_density)
-plot(env_vars_smooth5$tree_cover_density)
-
-writeRaster(env_vars, filename = "Data/all_covars_1km.grd", 
-            format = "raster", overwrite = T)
-writeRaster(env_vars_smooth3, filename = "Data/all_covars_1km_smooth3.grd", 
-            format = "raster", overwrite = T)
-writeRaster(env_vars_smooth5, filename = "Data/all_covars_1km_smooth5.grd", 
-            format = "raster", overwrite = T)
 
 
 ## sett model output as covar in the badger model
@@ -227,3 +91,97 @@ plot(env_vars)
 writeRaster(env_vars, file = "Data/Covars/final_covars_terra_with_setts.grd", overwrite = T)
 
 saveRDS(env_vars, file = "env_vars_with_setts.RDS")
+
+
+# # Resample rasters that are factors ####
+# 
+##  Land use ####
+# 
+# # 1 km
+# landUse <- env_vars$landCover
+# landUse_km <- projectRaster(landUse, crs = projKM, method = "ngb")
+# 
+# landCover <- as(landUse_km, "SpatialPixelsDataFrame")
+# landCover@data$landCover <- recode(landCover@data$landCover, 
+#                                    '1' = "Builtup", '2' = "Saltwater_related", 
+#                                    '3' = "No_vegetation", '4' = "Freshwater_related", 
+#                                    '5' = "Other_vegetation", '6' = "Agricultural", 
+#                                    '7' = "Pasture", '8' = "Grassland", '9' = "Transitional", 
+#                                    '10' = "Coniferous_forest", '11' = "Mixed_forest", 
+#                                    '12' = "Broadleaf_forest")
+# 
+# table(landCover@data$landCover)
+# 
+# saveRDS(landCover, file = "Data/landCover_1km.RDS")
+# 
+# # 5 km
+# 
+# landUse_5km <- resample(landUse_km, largerRas, method = "ngb")
+# landCover_5km <- as(landUse_5km, "SpatialPixelsDataFrame")
+# landCover_5km@data$landCover <- recode(landCover_5km@data$landCover, 
+#                                    '1' = "Builtup", '2' = "Saltwater_related", 
+#                                    '3' = "No_vegetation", '4' = "Freshwater_related", 
+#                                    '5' = "Other_vegetation", '6' = "Agricultural", 
+#                                    '7' = "Pasture", '8' = "Grassland", '9' = "Transitional", 
+#                                    '10' = "Coniferous_forest", '11' = "Mixed_forest", 
+#                                    '12' = "Broadleaf_forest")
+# 
+# 
+# table(landCover_5km@data$landCover)
+# 
+# saveRDS(landCover_5km, file = "Data/landCover_5km.RDS")
+# 
+##  Dominant leaf type ####
+# 
+# # 1 km
+# dlt <- env_vars$dominant_leaf_type
+# 
+# dlt_km <- projectRaster(dlt, crs = projKM, method = "ngb")
+# 
+# dlt_pix <- as(dlt_km, "SpatialPixelsDataFrame")
+# 
+# dlt_pix@data$dominant_leaf_type <- recode(dlt_pix@data$dominant_leaf_type, 
+#                                           '1' = "Broadleaf", '2' = "Coniferous")
+# 
+# table(dlt_pix@data$dominant_leaf_type)
+# 
+# saveRDS(dlt_pix, file = "Data/dlt_1km.RDS")
+# 
+# # 5 km
+# 
+# dlt_5km <- resample(dlt_km, largerRas, method = "ngb")
+# dlt_pix_5km <- as(dlt_5km, "SpatialPixelsDataFrame")
+# dlt_pix_5km@data$dominant_leaf_type <- recode(dlt_pix_5km@data$dominant_leaf_type, 
+#                                               '1' = "Broadleaf", '2' = "Coniferous")
+# 
+# 
+# table(dlt_pix_5km@data$dominant_leaf_type)
+# 
+# saveRDS(dlt_pix_5km, file = "Data/dlt_5km.RDS")
+# 
+##  Forest type ####
+# 
+# # 1 km
+# ft <- env_vars$forest_type
+# 
+# ft_km <- projectRaster(ft, crs = projKM, method = "ngb")
+# 
+# ft_pix <- as(ft_km, "SpatialPixelsDataFrame")
+# 
+# ft_pix@data$forest_type <- recode(ft_pix@data$forest_type, 
+#                                           '0' = "Non-forest",  '1' = "Broadleaf", '2' = "Coniferous")
+# 
+# table(ft_pix@data$forest_type, useNA = "always")
+# 
+# saveRDS(ft_pix, file = "Data/ft_1km.RDS")
+# 
+# # 5 km
+# 
+# ft_5km <- resample(ft_km, largerRas, method = "ngb")
+# ft_pix_5km <- as(ft_5km, "SpatialPixelsDataFrame")
+# ft_pix_5km@data$forest_type <- recode(ft_pix_5km@data$forest_type, 
+#                                       '0' = "Non-forest",  '1' = "Broadleaf", '2' = "Coniferous")
+# 
+# table(ft_pix_5km@data$forest_type, useNA = "always")
+# 
+# saveRDS(ft_pix_5km, file = "Data/ft_5km.RDS")
