3f1e5717d1e4054cb09f2e3b7c3f84e1ccfcbc68
Virginia <morera.virginia@gmail.com>
2026-06-17 18:33:04 +0100

    changes after discussing with david and andrew

--- Scripts/data_for_MS.R
+++ Scripts/data_for_MS.R
@@ -1,38 +1,33 @@
-# house keeping ####
+# Housekeeping ####
 rm(list = ls())
 source("Scripts/setup.R")
 
-quart <- read_sf("Data/Raw/just_quartiles.shp") %>% 
-  rename(QUARTILE = Q) %>% 
-  st_set_crs(29902) %>% 
-  st_transform(st_crs(2157))
+# Sett data
 
-ireland <- read_sf("Data/Other/Ireland_ITM.shp")
+sett_all <- readRDS("Data/sett_2025_inside_effort.RDS") 
 
-# Sett data ####
+sett_jittered <- sett_all %>% 
+  st_jitter(amount = 1000) %>% 
+  filter(MAIN_SETT == "Yes") 
 
-# Generalise ####
-sett <- readRDS("Data/sett_2025_inside_effort.RDS")
+st_write(sett_jittered, "sett_data_jittered.csv", layer_options = "GEOMETRY=AS_XY")
 
-main_setts_per_quartile <-  sett %>% 
-  filter(MAIN_SETT == "Yes") %>% 
-  st_intersection(quart) %>%  
-  st_drop_geometry() %>% 
-  group_by(QUARTILE) %>% 
-  summarise(n_setts = n_distinct(SETT_ID)) %>% 
-  select(QUARTILE, n_setts) %>% 
-  right_join(quart)  %>% 
-  st_as_sf(sf_column_name = "geometry") %>% 
-  select(-QUART)
 
-ggplot() + 
-  geom_sf(data = ireland, fill = NA, col = "black") + 
-  geom_sf(data = main_setts_per_quartile, aes(fill = n_setts), col = NA) + 
-  scale_fill_viridis_c(na.value = NA) + 
-  theme_bw()
-  
-# Randomise ####
+# Badger data ####
 
-randomised_main_setts <-  sett %>% 
-  filter(MAIN_SETT == "Yes")  %>% 
-  st_jitter(amount = 500)
+badgers_all <- readRDS("Data/badgers_thinned.RDS") 
+
+
+ireland_counties <- read_sf("Data/Other/Ireland_ITM.shp") %>% 
+  st_transform(st_crs(badgers_all)) %>% 
+  select(County_name = NAME_TAG)
+
+badgers_per_county <- st_join(badgers_all, ireland_counties) %>% 
+  group_by(County_name) %>%
+  summarise(n_badgers = n()) %>% 
+  st_drop_geometry %>% 
+  filter(County_name %!in% c("Antrim", "Armagh", "Down", "Fermanagh", 
+                             "Londonderry", "Tyrone")) %>% 
+  inner_join(ireland_counties)
+
+st_write(badgers_per_county, dsn = "badger_data_aggregated.shp")
